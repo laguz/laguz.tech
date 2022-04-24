03f60a8655ab9552109d195a10785b501e0d89a9
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2022-04-24 12:19:45 -0400

    I have the barebones to start building with Angularjs and Material design.

--- app.py
+++ app.py
@@ -1,115 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, flash, Markup, jsonify
 
-from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField, TextField,\
-    FormField, SelectField, FieldList
-from wtforms.validators import DataRequired, Length
-from wtforms.fields import *
-
-from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
-app.secret_key = 'dev'
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-
-# set default button sytle and size, will be overwritten by macro parameters
-app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
-app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
-# app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'  # uncomment this line to test bootswatch theme
-
-# set default icon title of table actions
-app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
-app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
-app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
-app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
-
-bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
-csrf = CSRFProtect(app)
-
-
-class ExampleForm(FlaskForm):
-    """An example form that contains all the supported bootstrap style form fields."""
-    date = DateField(description="We'll never share your email with anyone else.")  # add help text with `description`
-    datetime = DateTimeField(render_kw={'placeholder': 'this is placeholder'})  # add HTML attribute with `render_kw`
-    image = FileField(render_kw={'class': 'my-class'})  # add your class
-    option = RadioField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
-    select = SelectField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
-    selectmulti = SelectMultipleField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
-    bio = TextAreaField()
-    title = StringField()
-    secret = PasswordField()
-    remember = BooleanField('Remember me')
-    submit = SubmitField()
-
-
-class HelloForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
-    remember = BooleanField('Remember me')
-    submit = SubmitField()
-
-
-class ButtonForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
-    submit = SubmitField()
-    delete = SubmitField()
-    cancel = SubmitField()
-
-
-class TelephoneForm(FlaskForm):
-    country_code = IntegerField('Country Code')
-    area_code = IntegerField('Area Code/Exchange')
-    number = TextField('Number')
-
-
-class IMForm(FlaskForm):
-    protocol = SelectField(choices=[('aim', 'AIM'), ('msn', 'MSN')])
-    username = TextField()
-
-
-class ContactForm(FlaskForm):
-    first_name = TextField()
-    last_name = TextField()
-    mobile_phone = FormField(TelephoneForm)
-    office_phone = FormField(TelephoneForm)
-    emails = FieldList(TextField("Email"), min_entries=3)
-    im_accounts = FieldList(FormField(IMForm), min_entries=2)
-
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(100), nullable=False)
-    category = db.Column(db.String(100), nullable=False)
-    draft = db.Column(db.Boolean, default=False, nullable=False)
-    create_time = db.Column(db.Integer, nullable=False, unique=True)
-
-
-@app.before_first_request
-def before_first_request_func():
-    db.drop_all()
-    db.create_all()
-    for i in range(20):
-        m = Message(
-            text='Test message {}'.format(i+1),
-            author='Author {}'.format(i+1),
-            category='Category {}'.format(i+1),
-            create_time=4321*(i+1)
-            )
-        if i % 4:
-            m.draft = True
-        db.session.add(m)
-    db.session.commit()
-
+app.secret_key = 'LAGUZ'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Luis Alberto Guzman Zorrilla.'
 
 
 if __name__ == '__main__':
