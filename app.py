61637df566df7678a08544caeebbc868855d4cfc
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2022-04-30 00:21:42 -0400

    Finally added material design from google.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, flash, Markup, jsonify
+from flask import Flask, render_template, request, flash, Markup, jsonify, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'LAGUZ'
