3c07d6bdf3841998e24d998a3b06386ec60b84b5
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2022-05-10 20:46:05 -0400

    Finally working the docker file.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask, render_template, request, flash, Markup, jsonify, redirect, url_for
 import pymongo
 from pymongo import MongoClient
 import numpy as np
 import pandas as pd
+from dotenv import load_dotenv, find_dotenv
 
 #Database
-#client = MongoClient(uri)
+load_dotenv(find_dotenv())
+uri = os.environ.get("URI")
+client = MongoClient(uri)
+db_Option = client.Options
 
 app = Flask(__name__)
 app.secret_key = 'LAGUZ'
