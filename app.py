219c27c881f7bd1785232b8aa0f334c0bec67a91
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2023-03-21 20:41:30 -0400

    I have Chapter one working in the synology nas website.

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
-# -*- coding: utf-8 -*-
-import os
-from flask import Flask, render_template, request, flash, Markup, jsonify, redirect, url_for
-import pymongo
-from pymongo import MongoClient
-import numpy as np
-import pandas as pd
-from dotenv import load_dotenv, find_dotenv
-
-#This a test for the local import
-#from model import TDSession
-
-#Database
-load_dotenv(find_dotenv())
-uri = os.environ.get("URI")
-client = MongoClient(uri)
-db_Option = client.Options
-
-app = Flask(__name__)
-app.secret_key = 'LAGUZ'
-
-@app.route('/')
-def index():
-    return "Luis Guzman"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+from flask import Flask 
+  
+from config import Configuration  # import our configuration data. 
+  
+app = Flask(__name__) 
+app.config.from_object(Configuration)  # use values from our Config-uration object.
--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-# -*- coding: utf-8 -*-
-import os
-from flask import Flask, render_template, request, flash, Markup, jsonify, redirect, url_for
-import pymongo
-from pymongo import MongoClient
-import numpy as np
-import pandas as pd
-from dotenv import load_dotenv, find_dotenv
-
-#This a test for the local import
-#from model import TDSession
-
-#Database
-load_dotenv(find_dotenv())
-uri = os.environ.get("URI")
-client = MongoClient(uri)
-db_Option = client.Options
-
-app = Flask(__name__)
-app.secret_key = 'LAGUZ'
-
-@app.route('/')
-def index():
-    return "Luis Guzman"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+from app import app  # import our Flask app 
+import views 
+  
+if __name__ == '__main__': 
+    app.run()
