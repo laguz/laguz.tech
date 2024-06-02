ad856780128530c4b89e31e84d94441245e1d376
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2024-06-02 12:36:05 -0400

    Still working on MVC layout.

--- controllers/machineController.py
+++ controllers/machineController.py
@@ -1,35 +1,19 @@
-# -*- coding: utf-8 -*-
-import os
-from flask import Flask, render_template, request, flash, Markup, jsonify, redirect, url_for
-import pymongo
-from pymongo import MongoClient
-import numpy as np
-import pandas as pd
-from dotenv import load_dotenv, find_dotenv
+import json
+from models.machine import Inserttable, db
+from services.user_service import insert_logic, create_logic
 
-#Database
-load_dotenv(find_dotenv())
-uri = os.environ.get("URI")
-client = MongoClient(uri)
-db_Option = client.Stocks
-
-app = Flask(__name__)
-app.secret_key = 'LAGUZ'
-
-@app.route('/')
 def index():
-    """
-    Route to render the index.html template.
-
-    This function is responsible for handling the '/' route. It returns the rendered
-    'index.html' template.
-
-    Returns:
-        The rendered 'index.html' template.
-    """
-    # Render the 'index.html' template and return it
-    return render_template('index.html')
+    return {'status': 'OK',
+            'localhost:5000/machines/create': 'Create table in mysql database',
+            'localhost:5000/machines/insert': 'Insert data in mysql database table(Inserttable)'}
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+def create():
+    
+    create_logic()
+
+
+# insert data into table.
+def insert():
+    
+    insert_logic() 
